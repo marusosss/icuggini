738499d6f122a69e6a1bb66f6d741173ce9aaf1e
Andrea Passabi <andreapassabi6@gmail.com>
2026-09-18 09:27:07 +0200

    aggiunti i file Persona.java e Automobile.java

---
+++ src/Persona.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Persona {
+    private String nome;
+    private String codiceFiscale;
+    private LocalDate dataNascita;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+}
+
