38ee7096980728bde106a2fb15c4e62b4adfc5de
Andrea Passabi <andreapassabi6@gmail.com>
2026-09-18 09:48:05 +0200

    problemi risolti

--- src/Persona.java
+++ src/Persona.java
@@ -1,24 +1,26 @@
+import java.time.LocalDate;
+
 public class Persona {
     private String nome;
     private String cognome;
     private int eta;
     private LocalDate dataNascita;
+    private String ciaoTu = "miao te";
 
-}
+    public String getCiaoTu() {
+        return ciaoTu;
+    }
+
+    public void setCiaoTu(String ciaoTu) {
+        this.ciaoTu = ciaoTu;
+    }
+
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getCodiceFiscale() {
-        return codiceFiscale;
-    }
-
-    public void setCodiceFiscale(String codiceFiscale) {
-        this.codiceFiscale = codiceFiscale;
     }
 
     public LocalDate getDataNascita() {
@@ -29,11 +31,11 @@ public class Persona {
         this.dataNascita = dataNascita;
     }
     
-    public String getEta() {
+    public int getEta() {
         return eta;
     }
 
-    public void setEta(String eta) {
+    public void setEta(int eta) {
         this.eta = eta;
     }
 }
